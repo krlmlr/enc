ed198f1922da81812eb436c0e3d5dde31ca243b1
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-13 22:31:10 +0200

    rename test

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -4,6 +4,6 @@ test_that("output", {
   expect_identical(format(utf8(c("a", "bc"))), c("a ", "bc"))
 })
 
-test_that("output", {
+test_that("print", {
   expect_output_file(print(utf8(c("a", "bc"))), "output/abc.txt", update = TRUE)
 })
