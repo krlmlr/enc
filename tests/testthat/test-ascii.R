42612cbccb125a90d20f997ff9534afca0312ae2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-17 17:17:08 +0200

    test all_utf8()

--- tests/testthat/test-ascii.R
+++ tests/testthat/test-ascii.R
@@ -10,3 +10,13 @@ test_that("ASCII encoding detected works", {
     )),
     c("ASCII", "UTF-8", "latin1", "unknown"))
 })
+
+test_that("all_utf8()", {
+  expect_true(all_utf8(character()))
+  expect_true(all_utf8("a"))
+  expect_true(all_utf8(iconv("ä", to = "UTF-8")))
+  expect_true(all_utf8(c("a", iconv("ä", to = "UTF-8"))))
+  expect_false(all_utf8(iconv("ä", to = "latin1")))
+  expect_false(all_utf8(c("a", iconv("ä", to = "latin1"))))
+  expect_false(all_utf8(c("a", as_unknown("ä"))))
+})
