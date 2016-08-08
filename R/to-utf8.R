02c30be3024eaa157331c2954ba3eded6aa03d86
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-09 01:29:06 +0200

    to_utf8() converts to utf8 class

--- R/to-utf8.R
+++ R/to-utf8.R
@@ -14,8 +14,13 @@ to_utf8.list <- function(x, ...) {
   attrib_to_utf8(x)
 }
 
-to_utf8.character <- function(x, ...) {
-  x <- enc2utf8(x)
+to_utf8.data.frame <- to_utf8.list
+
+to_utf8.character <- function(x, ..., use_class = TRUE) {
+  if (use_class)
+    x <- as.utf8(x)
+  else
+    x <- enc2utf8(x)
   attrib_to_utf8(x)
 }
 
@@ -28,8 +33,18 @@ to_utf8.NULL <- function(x, ...) {
 }
 
 attrib_to_utf8 <- function(x) {
-  attrib <- attributes(x)
-  attrib[] <- to_utf8(unname(attrib))
-  mostattributes(x) <- attrib
+  mostattributes(x) <- named_to_utf8_except_class(attributes(x))
   x
+}
+
+named_to_utf8_except_class <- function(attrib) {
+  is_class <- which(names(attrib) == "class")
+  if (length(is_class) > 0) {
+    attrib[-is_class] <- to_utf8(unname(attrib)[-is_class])
+    attrib[[is_class]] <- to_utf8(unname(attrib)[[is_class]], use_class = FALSE)
+  } else {
+    attrib <- to_utf8(unname(attrib))
+  }
+
+  attrib
 }
