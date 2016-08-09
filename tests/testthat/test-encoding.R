96ffa01e75e0ba45d953193e173e2a6418981ea9
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-09 11:39:06 +0200

    update encoding test

--- tests/testthat/test-encoding.R
+++ tests/testthat/test-encoding.R
@@ -1,7 +1,9 @@
 context("encoding")
 
 test_that("encoding is always UTF-8", {
-  expect_equal(Encoding(utf8("ä")), "UTF-8")
-  expect_equal(Encoding(utf8(iconv("ä", to = "latin1"))), "UTF-8")
-  expect_equal(Encoding(utf8(iconv("ä", to = "utf-8"))), "UTF-8")
+  expect_equal(Encoding(utf8("\u00e4")), "UTF-8")
+  expect_equal(Encoding(utf8(enc2latin1("\u00e4"))), "UTF-8")
+  expect_equal(Encoding(utf8(enc2utf8("\u00e4"))), "UTF-8")
+  expect_equal(Encoding(utf8(c(enc2native("\u00e4"), enc2alien("\u00e4")))),
+               rep("UTF-8", 2))
 })
