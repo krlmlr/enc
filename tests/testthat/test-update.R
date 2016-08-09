3e6b0c90db0d9a502b2471ff9e2de32679e5d6f9
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-09 23:05:28 +0200

    use escape in test

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -1,33 +1,33 @@
 context("update")
 
 test_that("Update keeps encoding", {
-  x <- utf8(c("a", "ä"))
+  x <- utf8(c("a", "\u00e4"))
 
   x2 <- x
-  x2[2] <- "ä"
+  x2[2] <- "\u00e4"
   expect_identical(x, x2)
 
   x2 <- x
-  x2[2] <- iconv("ä", to = "latin1")
+  x2[2] <- iconv("\u00e4", to = "latin1")
   expect_identical(x, x2)
 
   x2 <- x
-  x2[2] <- iconv("ä", to = "utf8")
+  x2[2] <- iconv("\u00e4", to = "utf8")
   expect_identical(x, x2)
 
   x2 <- x
-  x2[[2]] <- "ä"
+  x2[[2]] <- "\u00e4"
   expect_identical(x, x2)
 
   x2 <- x
-  x2[[2]] <- iconv("ä", to = "latin1")
+  x2[[2]] <- iconv("\u00e4", to = "latin1")
   expect_identical(x, x2)
 
   x2 <- x
-  x2[[2]] <- iconv("ä", to = "utf8")
+  x2[[2]] <- iconv("\u00e4", to = "utf8")
   expect_identical(x, x2)
 })
 
 test_that("Concatenation keeps encoding", {
-  expect_is(c(utf8("ä"), "a"), "utf8")
+  expect_is(c(utf8("\u00e4"), "a"), "utf8")
 })
