7b86ea0be772d123246462b3e91cad0a31e2012f
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-09 10:41:29 +0200

    new to_encoding() used to implement to_utf8()

---
+++ R/to-utf8-encoding.R
@@ -0,0 +1,78 @@
+#' Deep conversion to an encoding
+#'
+#' Converts all characters directly or indirectly contained in an object to
+#' a specific encoding. \code{to_utf8} converts to
+#' UTF-8, using the \code{\link{utf8}} class where possible.
+#'
+#' @inheritParams base::Encoding
+#' @param ... passed on to methods
+#' @export
+#' @examples
+#' to_utf8(letters)
+#' to_utf8(iris)
+"to_encoding"
+
+#' @rdname to_encoding
+#' @export
+to_utf8 <- function(x, ...) to_encoding(x, ..., converter = utf8_converter)
+
+utf8_converter <- function(x, ..., use_class = TRUE) {
+  if (use_class)
+    as.utf8(x)
+  else
+    enc2utf8(x)
+}
+
+to_encoding <- function(x, ...) UseMethod("to_encoding", x)
+
+#' @export
+to_encoding.list <- function(x, ...) {
+  x[] <- lapply(x, to_encoding, ...)
+  names(x) <- to_encoding(names(x), ...)
+  attrib_to_encoding(x, ...)
+}
+
+#' @export
+to_encoding.data.frame <- function(x, ...) {
+  to_encoding.list(x, ...)
+}
+
+#' @rdname to_encoding
+#' @param converter A function that accepts a character value as first argument
+#' @export
+to_encoding.character <- function(x, ..., converter) {
+  x <- converter(x, ...)
+  attrib_to_encoding(x, converter = converter)
+}
+
+#' @export
+to_encoding.utf8 <- function(x, ...) {
+  to_encoding.default(x)
+}
+
+#' @export
+to_encoding.default <- function(x, ...) {
+  attrib_to_encoding(x, ...)
+}
+
+#' @export
+to_encoding.NULL <- function(x, ...) {
+  NULL
+}
+
+attrib_to_encoding <- function(x, ...) {
+  mostattributes(x) <- named_to_encoding_except_class(attributes(x), ...)
+  x
+}
+
+named_to_encoding_except_class <- function(attrib, ...) {
+  is_class <- which(names(attrib) == "class")
+  if (length(is_class) > 0) {
+    attrib[-is_class] <- to_encoding(unname(attrib)[-is_class], ...)
+    attrib[[is_class]] <- to_encoding(unname(attrib)[[is_class]], ..., use_class = FALSE)
+  } else {
+    attrib <- to_encoding(unname(attrib), ...)
+  }
+
+  attrib
+}
