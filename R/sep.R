75177426c5dec7dbf6ea5702ba807a94b3fc5c63
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-03 11:06:58 +0100

    add doc

--- R/sep.R
+++ R/sep.R
@@ -4,13 +4,17 @@ detect_sep <- function(path) {
 
   repeat {
     byte <- readBin(con, raw(1L))
-    if (length(byte) == 0) return(native_sep())
+    if (length(byte) == 0) return(native_eol())
     if (byte == 0x0d) return("\r\n")
     if (byte == 0x0a) return("\n")
   }
 }
 
-native_sep <- if (.Platform$OS.type == "windows") {
+#' The native end-of-line identifier on the current platform
+#'
+#' Returns CRLF on Windows and LF on Linux/OS X.
+#' @keywords internal
+native_eol <- if (.Platform$OS.type == "windows") {
   function() "\r\n"
 } else {
   function() "\n"
