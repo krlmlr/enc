1f7f4264b327a5abd95143f673a0b300e7665629
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-03 11:05:00 +0100

    give total number in ellipsis()

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 ellipsis <- function(x) {
   N <- 6
   if (length(x) > N) {
-    x <- c(x[seq_len(N)], "...")
+    x <- c(x[seq_len(N)], paste0("... (", length(x), " total)"))
   }
   paste(x, collapse = ", ")
 }
